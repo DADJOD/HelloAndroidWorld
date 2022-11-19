5dc641b8219bfb64697377ac9eafc06d6d76b0a8
DADJOD <kamu57@mail.ru>
2022-11-19 12:50:49 +0300

    First Commit
    First Commit

---
+++ app/src/main/java/King.java
@@ -0,0 +1,37 @@
+import android.view.View;
+import android.widget.Button;
+
+public class King {
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+//        Toast.makeText(this, "onCreate()", Toast
+//                .LENGTH_LONG).show();
+//    }
+
+//    Button button = (Button) findViewById(R.id.button);
+//
+
+//    WebView webView;
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+//
+//        webView = (WebView) findViewById(R.id.webview);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl("http://www.ya.ru");
+//
+//        webView.setWebViewClient(new WebViewSampleClient());
+//    }
+//    private class WebViewSampleClient extends WebViewClient {
+//        @Override
+//        public boolean shouldOverrideUrlLoading(WebView view,
+//                                                String url) {
+//            view.loadUrl(url);
+//            return true;
+//        }
+//    }
+
+}
